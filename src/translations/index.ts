c3bada49484816a672eebdc82266644e298f7781
Kicshikxo <supermegavalera9000@gmail.com>
2025-03-25 18:32:57 +0300

    refactor(structure): refactor translations structure

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -3,10 +3,10 @@ import ru from './ru'
 
 export { en, ru }
 
-export const getNamespacedTranslations = (translation: typeof en | typeof ru, namespaces: string[]) => {
+export const getNamespacedTranslations = (translations: typeof en | typeof ru, namespaces: string[]) => {
   return namespaces.reduce((acc, namespace) => {
-    if (namespace in translation) {
-      return { ...acc, ...(translation as Record<string, Record<string, string>>)[namespace] }
+    if (namespace in translations) {
+      return { ...acc, ...(translations as Record<string, Record<string, string>>)[namespace] }
     }
     return acc
   }, {} as Record<string, string>)
